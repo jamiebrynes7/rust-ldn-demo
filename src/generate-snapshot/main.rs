bae3ed8855804dee0ca130a7d6dc0cc5ed507cca
Jamie Brynes <jamiebrynes7@gmail.com>
2019-04-20 11:45:44 +0100

    add default snapshot

--- src/generate-snapshot/main.rs
+++ src/generate-snapshot/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    env,
+    path::PathBuf
+};
+use spatialos_sdk::worker::snapshot::SnapshotOutputStream;
+use structopt::StructOpt;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let opt :Opt = Opt::from_args();
+
+    let mut target_file = env::current_dir()?;
+    target_file.push(opt.snapshot_path);
+
+    let _stream = SnapshotOutputStream::new(target_file)?;
+
+    Ok(())
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "generate-snapshot")]
+struct Opt {
+
+    #[structopt(short = "p", long="snapshot-path")]
+    snapshot_path: PathBuf
 }
