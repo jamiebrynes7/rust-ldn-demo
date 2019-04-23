510e3e386d981e508724ccfee1c197cf70f99692
Jamie Brynes <jamiebrynes7@gmail.com>
2019-04-23 20:17:16 +0100

    add lumberjack and hq

--- src/shared/templates.rs
+++ src/shared/templates.rs
@@ -2,7 +2,7 @@ use spatialos_sdk::worker::entity_builder::EntityBuilder;
 use spatialos_sdk::worker::entity::Entity;
 use crate::shared::generated::improbable::Vector3d;
 use crate::shared::{GAMELOGIC_LAYER, CLIENT_LAYER};
-use crate::shared::generated::demo::Tree;
+use crate::shared::generated::demo::{Headquarters, Lumberjack, Tree, Action, ActionType};
 
 const TREE_RESOURCE_COUNT: u32 = 5;
 
@@ -12,8 +12,41 @@ pub fn tree(position: &Vector3d) -> Result<Entity, String> {
     builder.set_persistent(GAMELOGIC_LAYER);
     builder.set_entity_acl_write_access(GAMELOGIC_LAYER);
     builder.add_read_access(CLIENT_LAYER);
+
     builder.add_component(Tree {
         resources_left: TREE_RESOURCE_COUNT
+    }, GAMELOGIC_LAYER);
+
+    builder.build()
+}
+
+pub fn lumberjack(position: &Vector3d) -> Result<Entity, String> {
+    let mut builder = EntityBuilder::new(position.x, position.y, position.z, GAMELOGIC_LAYER);
+    builder.set_metadata("Lumberjack", GAMELOGIC_LAYER);
+    builder.set_persistent(GAMELOGIC_LAYER);
+    builder.set_entity_acl_write_access(GAMELOGIC_LAYER);
+    builder.add_read_access(CLIENT_LAYER);
+
+    builder.add_component(Lumberjack {
+        resources: 0,
+        action: Action {
+            typ: ActionType::IDLE,
+            target: None
+        }
+    }, GAMELOGIC_LAYER);
+
+    builder.build()
+}
+
+pub fn headquarters(position: &Vector3d) -> Result<Entity, String> {
+    let mut builder = EntityBuilder::new(position.x, position.y, position.z, GAMELOGIC_LAYER);
+    builder.set_metadata("Headquarters", GAMELOGIC_LAYER);
+    builder.set_persistent(GAMELOGIC_LAYER);
+    builder.set_entity_acl_write_access(GAMELOGIC_LAYER);
+    builder.add_read_access(CLIENT_LAYER);
+
+    builder.add_component(Headquarters {
+        score: 0
     }, GAMELOGIC_LAYER);
 
     builder.build()
