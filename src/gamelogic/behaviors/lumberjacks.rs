d31df4dad25301b11650dbaed40427613c317dea
Jamie Brynes <jamiebrynes@improbable.io>
2019-04-26 13:55:45 +0100

    more lumberjack

--- src/gamelogic/behaviors/lumberjacks.rs
+++ src/gamelogic/behaviors/lumberjacks.rs
@@ -2,7 +2,7 @@ use std::any::Any;
 use spatialos_sdk::worker::view::{View, ViewQuery};
 use spatialos_sdk::worker::connection::{WorkerConnection, Connection};
 use spatialos_sdk::worker::EntityId;
-use rust_ldn_demo::shared::generated::demo::{Lumberjack, ActionType, LumberjackUpdate, Action};
+use rust_ldn_demo::shared::generated::demo::{Lumberjack, ActionType, LumberjackUpdate, Action, Headquarters};
 use crate::behaviors::count_trees::TrackTreesBehaviour;
 use rust_ldn_demo::shared::generated::improbable::{Position, Coordinates, PositionUpdate};
 
@@ -33,6 +33,8 @@ impl LumberjackBehavior {
     }
 
     pub fn tick(&mut self, view: &View, connection: &mut WorkerConnection, trees: &TrackTreesBehaviour) {
+        let hqs = view.query::<HqQuery>().collect::<Vec<HqQuery>>();
+
         for lumberjack in view.query::<LumberjackQuery>() {
             let ljack = lumberjack.lumberjack;
             let pos = lumberjack.position.coords.clone();
@@ -56,24 +58,63 @@ impl LumberjackBehavior {
                 },
                 ActionType::FETCHING => {
                     let target = ljack.action.target.unwrap();
-                    let target_position = view.get_component::<Position>(target).unwrap();
+
+                    let target_position = match view.get_component::<Position>(target) {
+                        Some(c) => c,
+                        None => continue
+                    };
 
                     let dist_squared = squared_distance(&pos, &target_position.coords);
 
                     if dist_squared < 3.0 {
+                        let mut hq_id = hqs.iter()
+                            .map(|hq| {
+                                (hq.entity_id, squared_distance(&pos,&hq.position.coords))
+                            }).collect::<Vec<(EntityId, f64)>>();
+
+                        hq_id.sort_by(|a, b| {
+                            a.1.partial_cmp(&b.1).unwrap()
+                        });
+
+                        if hq_id.is_empty() {
+                            eprintln!("No hqs found?");
+                        }
+
                         connection.send_component_update::<Lumberjack>(lumberjack.entity_id, LumberjackUpdate {
                             resources: Some(1),
                             action: Some(Action {
                                 typ: ActionType::RETURNING,
-                                target: None
+                                target: Some(hq_id[0].0)
                             })
                         }, self.update_params.clone())
                     }
                     else {
-                        let mut position_change = normalized_direction(&target_position.coords, &pos);
-                        multiply(&mut position_change, MOVE_SPEED);
+                        let new_position = move_lumberjack(&pos, &target_position.coords);
 
-                        let new_position = add_coords(&pos, &position_change);
+                        connection.send_component_update::<Position>(lumberjack.entity_id, PositionUpdate {
+                            coords: Some(new_position)
+                        }, self.update_params.clone());
+                    }
+                },
+                ActionType::RETURNING => {
+                    let target = ljack.action.target.unwrap();
+
+                    let target_position = match view.get_component::<Position>(target) {
+                        Some(c) => c,
+                        None => continue
+                    };
+
+                    if squared_distance(&pos, &target_position.coords) < 3.0 {
+                        connection.send_component_update::<Lumberjack>(lumberjack.entity_id, LumberjackUpdate {
+                            resources: Some(0),
+                            action: Some(Action {
+                                typ: ActionType::IDLE,
+                                target: None
+                            })
+                        }, self.update_params.clone());
+                    }
+                    else {
+                        let new_position = move_lumberjack(&pos, &target_position.coords);
 
                         connection.send_component_update::<Position>(lumberjack.entity_id, PositionUpdate {
                             coords: Some(new_position)
@@ -84,6 +125,14 @@ impl LumberjackBehavior {
             }
         }
     }
+}
+
+
+fn move_lumberjack(from: &Coordinates, to: &Coordinates) -> Coordinates {
+    let mut position_change = normalized_direction(to, from);
+    multiply(&mut position_change, MOVE_SPEED);
+
+    add_coords(from, &position_change)
 }
 
 struct LumberjackQuery<'a> {
@@ -101,6 +150,24 @@ impl<'a, 'b: 'a> ViewQuery<'b> for LumberjackQuery<'a> {
         LumberjackQuery {
             entity_id,
             lumberjack: view.get_component::<Lumberjack>(entity_id).unwrap(),
+            position: view.get_component::<Position>(entity_id).unwrap()
+        }
+    }
+}
+
+struct HqQuery<'a> {
+    entity_id: EntityId,
+    position: &'a Position
+}
+
+impl<'a, 'b: 'a> ViewQuery<'b> for HqQuery<'a> {
+    fn filter(view: &View, entity_id: EntityId) -> bool {
+        view.get_component::<Headquarters>(entity_id).is_some() && view.get_component::<Position>(entity_id).is_some()
+    }
+
+    fn select(view: &'b View, entity_id: EntityId) -> Self {
+        HqQuery {
+            entity_id,
             position: view.get_component::<Position>(entity_id).unwrap()
         }
     }
